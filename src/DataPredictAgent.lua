99ed469d29c7bffe9ef0cc48adba55c66f0d32d2
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-11-18 19:24:38 +0800

    Update DataPredictAgent.lua

--- src/DataPredictAgent.lua
+++ src/DataPredictAgent.lua
@@ -1,3 +1,31 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's AI Agent Library (DataPredict-Agent)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
 local HttpService = game:GetService("HttpService")
 
 --------------------------------------------------------------------------------
