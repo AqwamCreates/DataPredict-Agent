73a4c568b47a871a11c67882d40df8fb6ed82ae3
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-11-18 18:41:31 +0800

    Create DataPredictAgentTest.lua

---
+++ src/DataPredictAgentTest.lua
@@ -0,0 +1,43 @@
+local DataPredictAgentModuleScript = game.ServerScriptService.DataPredictAgent
+
+local DataPredictAgentRequiredModuleScript = require(DataPredictAgentModuleScript)
+
+local HiddenPromptDictionary = require(DataPredictAgentModuleScript.HiddenPromptDictionary)
+
+local PersonalityDictionary = require(DataPredictAgentModuleScript.PersonalityDictionary)
+
+local DictionaryOfAgentActionDictionary = require(DataPredictAgentModuleScript.DictionaryOfAgentActionDictionary)
+
+local DataPredictAgent = DataPredictAgentRequiredModuleScript.new(true)
+
+local agentDictionary = {}
+
+local agentActionArray = {}
+
+local serverDictionary = {
+
+}
+
+for actionName, content in DictionaryOfAgentActionDictionary do
+	
+	DataPredictAgent:addAgentActionDictionary(actionName, content)
+	
+	table.insert(agentActionArray, actionName)
+	
+end
+
+agentDictionary.agentActionArray = agentActionArray
+
+agentDictionary.serverName = "default"
+
+agentDictionary.hiddenPrompt =  HiddenPromptDictionary["default"] .. PersonalityDictionary["seductive"] 
+
+DataPredictAgent:addServerDictionary("default", serverDictionary)
+
+DataPredictAgent:addAgentDictionary("Lilith", agentDictionary)
+
+DataPredictAgent:addInteractorDictionary("Aqwam")
+
+local response = DataPredictAgent:chat("Lilith", "Aqwam", "Hi!")
+
+print(response)
