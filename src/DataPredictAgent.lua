64676af97d4356ee54704edc01dc01633f59ef03
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-11-19 00:56:21 +0800

    Update DataPredictAgent.lua

--- src/DataPredictAgent.lua
+++ src/DataPredictAgent.lua
@@ -2,7 +2,7 @@
 
 	--------------------------------------------------------------------
 
-	Aqwam's AI Agent Library (DataPredict-Agent)
+	Aqwam's AI Agent Library (DataPredict Agent)
 
 	Author: Aqwam Harish Aiman
 	
