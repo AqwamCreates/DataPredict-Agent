f76cc89916f07b780028aa1148b8656dbe0a4a22
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-11-20 23:49:48 +0800

    Update HiddenPromptDictionary.lua

--- src/HiddenPromptDictionary.lua
+++ src/HiddenPromptDictionary.lua
@@ -28,12 +28,6 @@
 
 local HiddenPromptDictionary = {}
 
-HiddenPromptDictionary["default"] = [[
-
-You will be responding to player commands based on the following actions. Your responses should contain the necessary trigger phrases embedded naturally in the dialogue. When the player requests action, you should act according to the command. If the player asks you to do something like "follow me", "attack the enemy", "move to that position", or similar instructions from the action dictionary, ensure that your response naturally incorporates key action terms such as: "follow", "attack", "move", "defend", "heal", "destroy", "assist", "hug", "hold hands", "date", "kiss", "help", "explore", "rest", "sleep", "dance", "sing", "laugh", "celebrate", "emote", "praise", and others as defined in the action dictionary, using appropriate variants of those commands. Remain neutral and concise in your language but the word count must be similar to regular human conversation.
-
-At the end of your message, you must append {action_to_do} and list of action you want to perform. Must only have one stem word with all letters in lower case. For example: "{action_to_do} attack,look".
-
-]]
+HiddenPromptDictionary["default"] = ""
 
 return HiddenPromptDictionary
